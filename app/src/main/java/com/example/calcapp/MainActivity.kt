b077384605ea9dbe8aa1cc8955fdb3fa790d9fc7
kouki.kimura2 <pengin24.purupuru@gmail.com>
2019-08-21 21:39:14 +0900

    OK

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calcapp
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,41 @@ import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
-
+class MainActivity : AppCompatActivity() ,View.OnClickListener{
+    var operator = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab1.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab1.setOnClickListener {view ->
+            if (edit1.text.toString() != "" || edit2.text.toString() != ""){
+                operator = "+"
+                onClick(view)
+            }
+            else noText(view)
         }
-
+        fab2.setOnClickListener {view ->
+            if (edit1.text.toString() != "" || edit2.text.toString() != ""){
+                operator = "-"
+                onClick(view)
+            }
+            else noText(view)
+        }
+        fab3.setOnClickListener {view ->
+            if (edit1.text.toString() != "" || edit2.text.toString() != ""){
+                operator = "*"
+                onClick(view)
+            }
+            else noText(view)
+        }
+        fab4.setOnClickListener {view ->
+            if (edit1.text.toString() != "" || edit2.text.toString() != ""){
+                operator = "/"
+                onClick(view)
+            }
+            else noText(view)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,5 +61,18 @@ class MainActivity : AppCompatActivity(){
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent( this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", edit1.text.toString().toDouble())
+        intent.putExtra("VALUE2",edit2.text.toString().toDouble())
+        intent.putExtra("OPERATOR",operator)
+        startActivity(intent)
+    }
+
+    private fun noText(view: View){
+        Snackbar.make(view,"数値を入力してください", Snackbar.LENGTH_LONG)
+            .setAction("Action",null).show()
     }
 }
--- app/src/main/java/com/example/calcapp/SecondActivity.kt
+++ app/src/main/java/com/example/calcapp/SecondActivity.kt
@@ -2,11 +2,23 @@ package com.example.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val value1 = intent.getDoubleExtra("VALUE1", 0.0)
+        val value2 = intent.getDoubleExtra("VALUE2",0.0)
+
+        when(intent.getStringExtra("OPERATOR")){
+            "+" -> textView.text="${value1 + value2}"
+            "-" -> textView.text="${value1 - value2}"
+            "*" -> textView.text="${value1 * value2}"
+            "/" -> textView.text="${value1 / value2}"
+        }
     }
 }
